07cfa92701637e0fd306452becd21948a9900515
Jorge Eyzaguirre <jorgeyza@gmail.com>
2023-03-20 15:42:22 -0500

    Add dashboard page

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,17 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Ecomvision</title>
+        <meta name="description" content="View your data easily in this dashboard" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>hello</main>
+    </>
+  );
+};
+
+export default Home;
