e71906864203778942d5bca20d075a6edd372a31
Jorge Eyzaguirre <jorgeyza@gmail.com>
2023-03-20 15:25:54 -0500

    Add ColorModeScript

---
+++ src/pages/_document.tsx
@@ -0,0 +1,21 @@
+import { ColorModeScript, type ThemeConfig } from "@chakra-ui/react";
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+
+import theme from "~/theme";
+
+const themeConfig = theme.config as ThemeConfig;
+
+export default class Document extends NextDocument {
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <ColorModeScript initialColorMode={themeConfig.initialColorMode} />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
