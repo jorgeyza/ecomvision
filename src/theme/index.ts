584a9419d9b422b98902c8179e28c94f4d63ae68
Jorge Eyzaguirre <jorgeyza@gmail.com>
2023-03-22 16:27:54 -0500

    Generate theme semantic tokens

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -2,13 +2,8 @@ import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
 
 import { globalStyles as styles } from "./styles";
 
-const config: ThemeConfig = {
-  initialColorMode: "dark",
-  useSystemColorMode: false,
-};
-
-const colors = {
-  grey: {
+const tokensDark = {
+  neutral: {
     0: "#ffffff",
     10: "#f6f6f6",
     50: "#f0f0f0",
@@ -24,6 +19,9 @@ const colors = {
     1000: "#000000",
   },
   primary: {
+    0: "#ffffff",
+    10: "#ffffff",
+    50: "#ffffff",
     100: "#d3d4de",
     200: "#a6a9be",
     300: "#7a7f9d",
@@ -35,6 +33,8 @@ const colors = {
     900: "#070812",
   },
   secondary: {
+    0: "#ffffff",
+    10: "#ffffff",
     50: "#f0f0f0",
     100: "#fff6e0",
     200: "#ffedc2",
@@ -46,6 +46,113 @@ const colors = {
     800: "#665429",
     900: "#332a14",
   },
+} as const;
+
+const tokensLight = {
+  neutral: {
+    0: "#000000",
+    10: "#141414",
+    50: "#3d3d3d",
+    100: "#666666",
+    200: "#858585",
+    300: "#a3a3a3",
+    400: "#c2c2c2",
+    500: "#e0e0e0",
+    600: "#f0f0f0",
+    700: "#f6f6f6",
+    800: "#ffffff",
+    900: "#ffffff",
+    1000: "#ffffff",
+  },
+  primary: {
+    0: "#ffffff",
+    10: "#ffffff",
+    50: "#ffffff",
+    100: "#070812",
+    200: "#0d1025",
+    300: "#141937",
+    400: "#191F45",
+    500: "#21295c",
+    600: "#4d547d",
+    700: "#7a7f9d",
+    800: "#a6a9be",
+    900: "#d3d4de",
+  },
+  secondary: {
+    0: "#ffffff",
+    10: "#ffffff",
+    50: "#332a14",
+    100: "#665429",
+    200: "#997d3d",
+    300: "#cca752",
+    400: "#ffd166",
+    500: "#ffda85",
+    600: "#ffe3a3",
+    700: "#ffedc2",
+    800: "#fff6e0",
+    900: "#f0f0f0",
+  },
+} as const;
+
+/**
+ * Generates color semantic tokens from a set of dark and light color tokens.
+ * @param darkTokens - The set of dark color tokens.
+ * @param lightTokens - The set of light color tokens.
+ * @returns {Record<string, { _light: string; _dark: string }>} A record of color semantic tokens, keyed by semantic token name.
+ *
+ * @example
+ * // Generate color semantic tokens for the primary color
+ * const primaryTokens = generateColorSemanticTokens(tokensDark.primary, tokensLight.primary);
+ * // primaryTokens = {
+ * //   "primary-0": { _light: "#F4F4F4", _dark: "#000000" },
+ * //   "primary-10": { _light: "#E6E6E6", _dark: "#1A1A1A" },
+ * //   "primary-50": { _light: "#B3B3B3", _dark: "#4D4D4D" },
+ * //   ...
+ * // }
+ */
+
+function generateColorSemanticTokens(
+  darkTokens: typeof tokensDark,
+  lightTokens: typeof tokensLight
+) {
+  type Tokens = typeof tokensDark;
+  type ColorKey = keyof Tokens;
+  type ShadeKey = keyof Tokens["neutral"];
+
+  const colorKeys = Object.keys(tokensDark) as ColorKey[];
+
+  const result: Record<string, { _light: string; _dark: string }> = {};
+
+  colorKeys.forEach((colorKey) => {
+    // convert each string key to a number to have correct assertion for TS
+    const shades = Object.keys(darkTokens[colorKey]).map(Number) as Array<
+      keyof (typeof darkTokens)[typeof colorKey] & ShadeKey
+    >;
+
+    shades.forEach((shadeKey) => {
+      const lightColor = lightTokens[colorKey][shadeKey];
+      const darkColor = darkTokens[colorKey][shadeKey];
+
+      const semanticTokenKey = `${colorKey}-${shadeKey}`;
+
+      result[semanticTokenKey] = {
+        _light: lightColor,
+        _dark: darkColor,
+      };
+    });
+  });
+
+  return result;
+}
+
+const colorsSemanticTokens = generateColorSemanticTokens(
+  tokensDark,
+  tokensLight
+);
+
+const config: ThemeConfig = {
+  initialColorMode: "dark",
+  useSystemColorMode: false,
 };
 
 const fonts = {
@@ -55,7 +162,9 @@ const fonts = {
 
 const theme = extendTheme({
   config,
-  colors,
+  semanticTokens: {
+    colors: colorsSemanticTokens,
+  },
   fonts,
   styles,
 });
