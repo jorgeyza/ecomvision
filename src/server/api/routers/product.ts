233989dd683f432fe1cfeb68365637ee394b7dac
Jorge Eyzaguirre <jorgeyza@gmail.com>
2023-04-05 16:57:45 -0500

    Update product router

--- src/server/api/routers/product.ts
+++ src/server/api/routers/product.ts
@@ -2,6 +2,8 @@ import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const productRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.product.findMany();
+    return ctx.prisma.product.findMany({
+      include: { productStat: true },
+    });
   }),
 });
