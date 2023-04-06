370f7bb9c391e51b5c0628f1185a859057df1026
Jorge Eyzaguirre <jorgeyza@gmail.com>
2023-04-05 23:22:21 -0500

    Update dashboard page

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,17 +1,36 @@
+import { Button, Flex } from "@chakra-ui/react";
+import { Download } from "lucide-react";
 import { type NextPage } from "next";
 import Head from "next/head";
 
-const Home: NextPage = () => {
+import PageHeadings from "~/components/ui/PageHeadings";
+
+const Dashboard: NextPage = () => {
   return (
     <>
       <Head>
         <title>Ecomvision</title>
-        <meta name="description" content="View your data easily in this dashboard" />
+        <meta
+          name="description"
+          content="View your data easily in this dashboard"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>HERE GOES THE DASHBOARD</div>
+      <Flex width="100%" alignItems="center" justifyContent="space-between">
+        <PageHeadings title="DASHBOARD" subtitle="Welcome to your dashboard" />
+        <Button
+          columnGap={3}
+          backgroundColor="accent-200"
+          color="primary-500"
+          size="lg"
+          fontSize="sm"
+        >
+          <Download size={20} />
+          DOWNLOAD REPORTS
+        </Button>
+      </Flex>
     </>
   );
 };
 
-export default Home;
+export default Dashboard;
