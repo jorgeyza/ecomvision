1b354a0a6a2a43009e994c9db75f545f3f42b439
Jorge Eyzaguirre <jorgeyza@gmail.com>
2023-04-18 19:49:22 -0500

    Add geography page

--- src/pages/geography.tsx
+++ src/pages/geography.tsx
@@ -1,28 +1,121 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, useToken } from "@chakra-ui/react";
+import { ResponsiveChoropleth } from "@nivo/geo";
 import { type NextPage } from "next";
 
 import Loading from "~/components/ui/Loading";
 import PageHeadings from "~/components/ui/PageHeadings";
 
 import { api } from "~/utils/api";
+import { geoData } from "~/utils/geoData";
 
 const Geography: NextPage = () => {
+  const [accent100, accent200, primary500, neutral300] = useToken("colors", [
+    "accent-100",
+    "accent-200",
+    "primary-500",
+    "neutral-200",
+  ]);
+
   const {
-    data: allCustomers,
+    data: usersByCountry = [],
     status,
     error,
-  } = api.user.getAllWithUserRole.useQuery();
-  console.log("🚀 ~ file: products.tsx:25 ~ allProducts:", allCustomers);
+  } = api.user.getGeography.useQuery();
+
   return (
     <>
-      <PageHeadings title="CUSTOMERS" subtitle="List of customers" />
+      <PageHeadings
+        title="GEOGRAPHY"
+        subtitle="Find where your users are located"
+      />
       <Flex width="100%" flexDirection="column" alignItems="center">
         {status === "loading" ? (
           <Loading />
         ) : status === "error" ? (
           <p>Error {error.message}</p>
         ) : (
-          <p>Here goes the table</p>
+          <Flex
+            height="75vh"
+            width="100%"
+            border="1px solid"
+            borderColor="accent-200"
+            backgroundColor="background-emphasis"
+            borderRadius="4px"
+          >
+            <ResponsiveChoropleth
+              data={usersByCountry}
+              theme={{
+                axis: {
+                  domain: {
+                    line: {
+                      stroke: accent200,
+                    },
+                  },
+                  legend: {
+                    text: {
+                      fill: accent200,
+                    },
+                  },
+                  ticks: {
+                    line: {
+                      stroke: accent200,
+                      strokeWidth: 1,
+                    },
+                    text: {
+                      fill: accent200,
+                    },
+                  },
+                },
+                legends: {
+                  text: {
+                    fill: accent200,
+                  },
+                },
+                tooltip: {
+                  container: {
+                    color: primary500,
+                  },
+                },
+              }}
+              colors="greens"
+              features={geoData.features}
+              margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
+              domain={[0, 60]}
+              unknownColor="#666666"
+              label="properties.name"
+              valueFormat=".2s"
+              projectionScale={150}
+              projectionTranslation={[0.5, 0.6]}
+              projectionRotation={[0, 0, 0]}
+              borderWidth={1.3}
+              borderColor={neutral300}
+              legends={[
+                {
+                  anchor: "bottom-right",
+                  direction: "column",
+                  justify: true,
+                  translateX: -25,
+                  translateY: -75,
+                  itemsSpacing: 0,
+                  itemWidth: 94,
+                  itemHeight: 18,
+                  itemDirection: "left-to-right",
+                  itemTextColor: accent200,
+                  itemOpacity: 0.85,
+                  symbolSize: 18,
+                  effects: [
+                    {
+                      on: "hover",
+                      style: {
+                        itemTextColor: accent100,
+                        itemOpacity: 1,
+                      },
+                    },
+                  ],
+                },
+              ]}
+            />
+          </Flex>
         )}
       </Flex>
     </>
--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,3 +1,5 @@
+import iso from "iso-3166-1";
+
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const userRouter = createTRPCRouter({
@@ -19,5 +21,34 @@ export const userRouter = createTRPCRouter({
         _count: true,
       },
     });
+  }),
+  getGeography: publicProcedure.query(async ({ ctx }) => {
+    const users = await ctx.prisma.user.findMany();
+
+    const mappedLocations = users.reduce<Record<string, number>>(
+      (acc, { country }) => {
+        if (!country) return acc;
+
+        const countryISO3 = iso.whereAlpha2(country);
+        if (!countryISO3) return acc;
+
+        const { alpha3 } = countryISO3;
+
+        if (!acc[alpha3]) {
+          acc[alpha3] = 0;
+        }
+        acc[alpha3]++;
+        return acc;
+      },
+      {}
+    );
+
+    const formattedLocations = Object.entries(mappedLocations).map(
+      ([country, count]) => {
+        return { id: country, value: count };
+      }
+    );
+
+    return formattedLocations;
   }),
 });
