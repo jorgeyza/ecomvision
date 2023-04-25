7493ffde6b16b448a2c6a55c6b5a931a5cd05931
Jorge Eyzaguirre <jorgeyza@gmail.com>
2023-04-24 20:06:33 -0500

    Update dashboard page

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,10 +1,23 @@
-import { Button, Flex, Heading } from "@chakra-ui/react";
-import { DownloadIcon } from "lucide-react";
+import { Button, Flex, Grid, GridItem, Heading } from "@chakra-ui/react";
+import { DownloadIcon, MailIcon, CalculatorIcon } from "lucide-react";
 import { type NextPage } from "next";
 
+import BreakdownChart from "~/components/BreakdownChart";
+import OverviewChart from "~/components/OverviewChart";
+import StatBox from "~/components/StatBox";
+import Loading from "~/components/ui/Loading";
+
 import PageHeadings from "~/components/ui/PageHeadings";
+import { api } from "~/utils/api";
+import { viewEnum } from "./overview";
 
 const Dashboard: NextPage = () => {
+  const {
+    data: dashboardStats,
+    status,
+    error,
+  } = api.overallStats.getDashboardStats.useQuery();
+
   return (
     <>
       <Flex width="100%" alignItems="center" justifyContent="space-between">
@@ -20,11 +33,54 @@ const Dashboard: NextPage = () => {
           DOWNLOAD REPORTS
         </Button>
       </Flex>
+      <Grid
+        autoRows="160px"
+        templateColumns="repeat(12, 1fr)"
+        gap={5}
+        width="100%"
+      >
+        {status === "loading" ? (
+          <Loading />
+        ) : status === "error" ? (
+          <p>Error {error.message}</p>
+        ) : !dashboardStats ? (
+          <p>No data</p>
+        ) : (
+          <>
+            <StatBox
+              title="Total Customers"
+              value={dashboardStats.totalCustomers}
+              increase="+14%"
+              description="Since last month"
+              icon={<MailIcon size={26} />}
+            />
+            <StatBox
+              title="Sales Today"
+              value={dashboardStats.todayStats.totalSales}
+              increase="+21%"
+              description="Since last month"
+              icon={<CalculatorIcon size={26} />}
+            />
+            <GridItem
+              gridColumn="span 8"
+              gridRow="span 2"
+              backgroundColor="background-emphasis"
+              p={4}
+              borderRadius="md"
+            >
+              <OverviewChart
+                data={dashboardStats.overallStat}
+                isDashboard={true}
+                view={viewEnum.SALES}
+              />
+            </GridItem>
+          </>
+        )}
+      </Grid>
       <Heading>SITE UNDER CONSTRUCTION...</Heading>
       <p style={{ fontSize: "24px" }}>Pending</p>
       <ul>
         <li>Dashboard</li>
-        <li>Performance</li>
       </ul>
     </>
   );
--- src/server/api/routers/overallstats.ts
+++ src/server/api/routers/overallstats.ts
@@ -4,4 +4,43 @@ export const overallStatsRouter = createTRPCRouter({
   getSales: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.overallStats.findFirst();
   }),
+  getDashboardStats: publicProcedure.query(async ({ ctx }) => {
+    // hardcoded values
+    const currentMonth = "November";
+    const currentYear = 2021;
+    const currentDay = "2021-11-15";
+
+    /* Overall Stats */
+    const overallStat = await ctx.prisma.overallStats.findFirst({
+      where: { year: currentYear },
+    });
+
+    const currentMonthStats = overallStat?.monthlyData.find(({ month }) => {
+      return month === currentMonth;
+    });
+
+    const todayStats = overallStat?.dailyData.find(({ date }) => {
+      return date === currentDay;
+    });
+
+    if (!overallStat || !currentMonthStats || !todayStats) return undefined;
+
+    /* Recent Transactions */
+    const transactions = await ctx.prisma.transaction.findMany({
+      take: 50,
+      orderBy: { createdAt: "desc" },
+    });
+
+    return {
+      overallStat,
+      totalCustomers: overallStat.totalCustomers,
+      yearlyTotalSoldUnits: overallStat.yearlyTotalSoldUnits,
+      yearlySalesTotal: overallStat.yearlySalesTotal,
+      monthlyData: overallStat.monthlyData,
+      salesByCategory: overallStat.salesByCategory,
+      currentMonthStats,
+      todayStats,
+      transactions,
+    };
+  }),
 });
